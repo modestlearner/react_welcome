d16dfc6a61671f7e890a86ccd613d56833224eb3
modestlearner <hj03111996@gmail.com>
2019-01-14 15:53:13 +0530

    op

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import Header from "./components/header";
 import { Route, Switch } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Info from "./components/info";
-import { HashRouter } from 'react-router-dom'
 
 const r = (
   <Router>
